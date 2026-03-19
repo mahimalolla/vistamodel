8f7bbf461f4c7234c201afa5b28f4cd71eee5533
henil9794 <henilpatel9794@gmail.com>
2026-03-18 22:25:15 -0400

    Updated Airflow DAGS pipeline

--- dags/offline_pipeline.py
+++ dags/offline_pipeline.py
@@ -12,6 +12,7 @@ default_args = {
     "email_on_failure": False,
 }
 
+def node0(): from dvc_pull import dvc_pull_or_download; dvc_pull_or_download()
 def node1(): from download_datasets import download_datasets; download_datasets()
 def node2(): from preprocess import preprocess_datasets; preprocess_datasets()
 def node3(): from split_dataset import split_dataset; split_dataset()
@@ -21,6 +22,7 @@ def node6(): from anomaly_detection import detect_anomalies; detect_anomalies()
 def node7(): from dataset_slicing import create_slices; create_slices()
 def node8(): from dataset_approval import approve_dataset; approve_dataset()
 def node9(): from upload_gcs import upload_to_gcs; upload_to_gcs()
+def node_dvc_push(): from dvc_push import dvc_push_datasets; dvc_push_datasets()
 def node10(): from trigger_online import trigger_online_pipeline; trigger_online_pipeline()
 
 with DAG(
@@ -33,15 +35,17 @@ with DAG(
     tags=["mlops", "translation", "offline"],
 ) as dag:
 
-    t1  = PythonOperator(task_id="node1_download_datasets",     python_callable=node1)
-    t2  = PythonOperator(task_id="node2_preprocess",            python_callable=node2)
-    t3  = PythonOperator(task_id="node3_train_val_test_split",  python_callable=node3)
-    t4  = PythonOperator(task_id="node4_ge_statistics",         python_callable=node4)
-    t5  = PythonOperator(task_id="node5_schema_inference",      python_callable=node5)
-    t6  = PythonOperator(task_id="node6_anomaly_detection",     python_callable=node6)
-    t7  = PythonOperator(task_id="node7_dataset_slicing",       python_callable=node7)
-    t8  = PythonOperator(task_id="node8_dataset_approval",      python_callable=node8)
-    t9  = PythonOperator(task_id="node9_upload_to_gcs",         python_callable=node9)
+    t0  = PythonOperator(task_id="node0_dvc_pull_or_download",    python_callable=node0)
+    t1  = PythonOperator(task_id="node1_download_datasets",        python_callable=node1)
+    t2  = PythonOperator(task_id="node2_preprocess",               python_callable=node2)
+    t3  = PythonOperator(task_id="node3_train_val_test_split",     python_callable=node3)
+    t4  = PythonOperator(task_id="node4_ge_statistics",            python_callable=node4)
+    t5  = PythonOperator(task_id="node5_schema_inference",         python_callable=node5)
+    t6  = PythonOperator(task_id="node6_anomaly_detection",        python_callable=node6)
+    t7  = PythonOperator(task_id="node7_dataset_slicing",          python_callable=node7)
+    t8  = PythonOperator(task_id="node8_dataset_approval",         python_callable=node8)
+    t9  = PythonOperator(task_id="node9_upload_to_gcs",            python_callable=node9)
+    t_dvc = PythonOperator(task_id="node9b_dvc_push",              python_callable=node_dvc_push)
     t10 = PythonOperator(task_id="node10_trigger_online_pipeline", python_callable=node10)
 
-    t1 >> t2 >> t3 >> t4 >> t5 >> t6 >> t7 >> t8 >> t9 >> t10
+    t0 >> t1 >> t2 >> t3 >> t4 >> t5 >> t6 >> t7 >> t8 >> t9 >> t_dvc >> t10
