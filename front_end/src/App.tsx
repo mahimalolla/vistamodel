98f72f0e91fc580df09cb7aa05f268849e3c5e16
Mahima Lolla <74255400+mahimalolla@users.noreply.github.com>
2026-04-14 22:52:36 -0400

    Update App.tsx

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Index from "./pages/Index";
+import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/translate" element={<Index />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
